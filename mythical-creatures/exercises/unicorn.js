524561c5abbbf9a506f167e696d5b30666b064f9
Cameron Aragon <caragon4695@gmail.com>
2020-10-13 22:05:25 -0600

    Add Unicorn & Vampire Work

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,27 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
+      this.color = color;
+    }
+  }
+
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    }
+  }
+
+  says(message) {
+    return `**;* ${message} *;**`;
   }
 }
 
 module.exports = Unicorn;
+
+// 1. Read the test
+// 2. Ask yourself if its going to fail and why
+// 3. Run the test
+// 4. Make it pass
